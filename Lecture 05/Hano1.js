dbf07eb36e864ef86ada473401d3fcd2eeb7dae8
Godmode59 <jasonkim226@gmail.com>
2022-04-13 16:00:33 +0900

    임시

--- Lecture 05/Hano1.js
+++ Lecture 05/Hano1.js
@@ -1,6 +1,25 @@
 function init() {
+    var arr = [];
+
+    arr.push("🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥");
+    arr.push("🟨🟨🟨🟨🟨🟨🟨🟨🟨");
+    arr.push("🟩🟩🟩🟩🟩🟩🟩");
+    arr.push("🟦🟦🟦🟦🟦");
+    arr.push("🟪🟪🟪");
+    arr.push("⬛️");
+
     const element =
     document.getElementById('first');
     element.innerHTML
     = '<div>   🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥 <br> 🟨🟨🟨🟨🟨🟨🟨🟨🟨<br>🟩🟩🟩🟩🟩🟩🟩<br>🟦🟦🟦🟦🟦<br>🟪🟪🟪 <br> ⬛️</div>';
+}
+function movingHectic() {
+    if(){
+
+    }else if(){
+
+    }
+    else(){
+
+    }
 }
